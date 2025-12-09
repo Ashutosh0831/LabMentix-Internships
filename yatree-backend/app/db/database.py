778964e9e85cb45a2bd241292b57d9c5353de58f
Ashutosh0831 <163403936+Ashutosh0831@users.noreply.github.com>
2025-12-09 12:37:54 +0530

    Update database.py

--- yatree-backend/app/db/database.py
+++ yatree-backend/app/db/database.py
@@ -19,21 +19,4 @@ SessionLocal = sessionmaker(
 )
 
 Base = declarative_base()
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
 
-
-DATABASE_URL = "postgresql://postgres:ashu%40123@localhost:5432/yatree-cab"
-
-engine = create_engine(
-    DATABASE_URL,
-    pool_pre_ping=True
-)
-
-SessionLocal = sessionmaker(
-    bind=engine,
-    autoflush=False,
-    autocommit=False
-)
-
-Base = declarative_base()
